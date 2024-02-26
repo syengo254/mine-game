b65c21e20b0ee44ca069e9a8e6a796fbe73df4ef
David Syengo <d.syengo@institute.global>
2024-02-26 11:25:19 +0300

    wip: added webpack + config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Game from "./src/game.js";
+import Game from "./game.js";
 
 const GAME_WIDTH = 600;
 const GAME_HEIGHT = 450;
@@ -7,7 +7,7 @@ const game = new Game(GAME_WIDTH, GAME_HEIGHT);
 const canvas = document.getElementById('stage');
 const ctx = canvas.getContext("2d");
 
-function mainLoop(dt){
+function mainLoop(dt) {
     ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     game.update(dt);
     game.draw(ctx);
